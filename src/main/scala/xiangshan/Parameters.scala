73b3c517f66b416e95c00a56cf67ad86718c61e8
xiaofeibao-xjtu <1441675923@qq.com>
2023-12-01 16:00:36 +0800

    backend: remove renameOut pipeline

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -380,7 +380,7 @@ case class XSCoreParameters
   def iqWakeUpParams = {
     Seq(
       WakeUpConfig(
-        Seq("ALU0", "ALU1", "MUL0", "MUL1", "BJU0", "LDU0", "LDU1") ->
+        Seq("ALU0", "ALU1", "MUL0", "MUL1", "LDU0", "LDU1") ->
         Seq("ALU0", "ALU1", "MUL0", "MUL1", "BJU0", "BJU1", "BJU2", "LDU0", "LDU1", "STA0", "STA1", "STD0", "STD1")
       ),
       WakeUpConfig(Seq("IMISC0") -> Seq("VFEX0")),
